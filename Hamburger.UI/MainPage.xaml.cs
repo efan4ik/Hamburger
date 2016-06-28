c6a605fab9dc56f552cf6a2f38577757f2c6d382
Ziv <ziv.nachum@gmail.com>
2016-06-28 11:42:41 +0300

    Revert "windows 10 frame&page navigation"
    This reverts commit fd91ad77679e48a7700afd06783efff026a3a203.

--- Hamburger.UI/MainPage.xaml.cs
+++ Hamburger.UI/MainPage.xaml.cs
@@ -14,7 +14,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Esri.ArcGISRuntime.Controls;
-using Hamburger.UI.Views;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -28,7 +27,11 @@ namespace Hamburger.UI
         public MainPage()
         {
             InitializeComponent();
-            MainFrame.Navigate(typeof(Views.MapView));
+            var frames = FramesContainer.Children.Where(c => c is Frame).Cast<Frame>();
+            foreach (var frame in frames)
+            {
+                _frames.Add(frame.Name, frame);
+            }
         }
 
         #region properties
@@ -51,36 +54,58 @@ namespace Hamburger.UI
 
         #endregion
 
+        private Dictionary<string, Frame> _frames = new Dictionary<string, Frame>();
+        public Dictionary<string, Frame> Frames
+        {
+            get { return _frames; }
+            set
+            {
+                _frames = value;
+            }
+        }
+
+
+        private string _currentFrame;
+        public string CurrentFrame
+        {
+            get { return _currentFrame; }
+            set
+            {
+                if (_currentFrame == null)
+                    _currentFrame = "MapFrame";
+
+                if (!Frames.ContainsKey(value))
+                    return;
+
+                Frames[_currentFrame].Visibility = Visibility.Collapsed;
+                _currentFrame = value;
+                Frames[_currentFrame].Visibility = Visibility.Visible;
+            }
+        }
 
         private void ButtonClick(object sender, RoutedEventArgs e)
         {
-            var page = (e.OriginalSource as Button).CommandParameter.ToString();
-            if (page == "Map")
-            {
-                MainFrame.Navigate(typeof(Views.MapView));
-            }
-            else if (page == "Text")
-            {
-                MainFrame.Navigate(typeof(TextView));
-            }
-            else if (page == "Image")
-            {
-                MainFrame.Navigate(typeof(ImageView));
-            }
+            CurrentFrame = (e.OriginalSource as Button).CommandParameter.ToString();
         }
 
         private void LeftHamburgerButton_Click(object sender, RoutedEventArgs e)
         {
-            //LeftPanel.IsPaneOpen = !LeftPanel.IsPaneOpen;
+            LeftPanel.IsPaneOpen = !LeftPanel.IsPaneOpen;
         }
 
         private void RightHamburgerButton_Click(object sender, RoutedEventArgs e)
         {
-            //RightPanel.IsPaneOpen = !RightPanel.IsPaneOpen;
+            RightPanel.IsPaneOpen = !RightPanel.IsPaneOpen;
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //if (MapListItem.IsSelected)
+            //    CurrentFrame = "MapFrame";
+            //else if (TextListItem.IsSelected)
+            //    CurrentFrame = "TextFrame";
+            //else if (ImageListItem.IsSelected)
+            //    CurrentFrame = "ImageFrame";
         }
     }
 }
